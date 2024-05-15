4d70e26fb0e23361e696985cde746af713a16514
Ammar Ahmed <ammar69aa59@gmail.com>
2024-05-14 23:09:00 -0700

    practice

---
+++ app.js
@@ -0,0 +1,33 @@
+// let obj = {
+//     firstName: 'Ammar',
+//     lastName: 'Ahmed',
+//     ID: 227780,
+//     enroll: 'WMA',
+//     email: 'ammar69aa59@gmail.com',
+//     topic: ['html ', 'css ', 'js'],
+//     bookDetails: {
+//         Aurthor: 'Alice Heprew',
+//         bookName: 'Smater Way To Learn JS'
+//     },
+//     getBookName: function () {
+//         return (`We are using the book to study is ${obj.bookDetails.bookName} and the Aurthor of this book is ${obj.bookDetails.Aurthor}`);
+//     },
+//     getTopic: function () {
+//         return (`Our course outline is ${obj.topic}`)
+//     },
+//     getFullName: function () {
+//         return (`Your client full name is : ${obj.firstName} ${obj.lastName}`)
+//     }
+// };
+// console.log(obj.getFullName());
+// console.log(obj.getTopic());
+// console.log(obj.getBookName());
+// obj.class = 'web' //adds in obj (object)
+// console.log(obj.bookDetails);
+// delete obj.enroll // delete 
+// console.log('firstName' in obj); //Check if the value is in the object or not .
+//  for (let key in obj) {
+//     console.log(key + " =====> " + obj[key]);
+//  }
+// obj[keys] it returs us the value stored in the object
+// key  it gives us the variable in the object;
