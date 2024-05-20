920df2d7dcfa4c6483caab5e66c5e82f1f35f85b
Ammar Ahmed <ammar69aa59@gmail.com>
2024-05-20 00:48:02 -0700

    obj-costructor concept covered

--- app.js
+++ app.js
@@ -1,38 +1,50 @@
 
 // let userInput= prompt('Enter mobile name').toLowerCase();
-// let mobile ={
-//     iphone :{
-//         model: '15 pro max',
-//         camera: ' 108 mp',
-//         ram:'20GB',
-//         battey: '6000 MaH',
-//         price : '7 lakh'
-//     },
-//     infinix :{
-//         model: 'Infinix Hot 11 play',
-//         camera: '64 mp',
-//         ram:'4 GB',
-//         battey: '7000 MaH',
-//         price : '20,000'
-//     },
-//     getModelName:function(){
-//         return this.iphone.model;
-//     },getBrandName:function(){
-//         return this.iphone.company;
-//     },
+let mobile = {
+    iphone: {
+        model: '15 pro max',
+        camera: ' 108 mp',
+        ram: '20GB',
+        battey: '6000 MaH',
+        price: '7 lakh'
+    },
+    infinix: {
+        model: 'Infinix Hot 11 play',
+        camera: '64 mp',
+        ram: '4 GB',
+        battey: '7000 MaH',
+        price: '20,000'
+    },
+    getModelName: function () {
+        return this.iphone.model;
+    }, getBrandName: function () {
+        return this.iphone.company;
+    },
+};
+// Js have constructor property to control the object properties name which are repeating
 
-
-// };
-// mobile.iphone.company = 'Iphone';
-// console.log(`Brand is ${mobile.getBrandName()} and  model is ${mobile.getModelName()}`);
-
-
-
-
-
-
-
-
+function Mobile(model,price,camera,ram,battey) {
+    this.model = model;
+    this.price = price;
+    this.camera = camera;
+    this.ram = ram;
+    this.battey = battey;
+    
+};
+//In constructor function name should be in variable with the keyword of new otherwise if we call this constructor function in this way so it will give undefined .
+// console.log(Mobile());
+var mobile1 = new Mobile('Q mobile',4000, '8mp','2GB','3500MaH');
+console.log(mobile1);
+var mobile2 = new Mobile('Samsung','40,000 PKR', '108mp','16GB','5000MaH');
+console.log(mobile2);
+var mobile3 = new Mobile('Iphone 8+','56000PKR', '4mp','4GB','4500MaH');
+console.log(mobile3);
+for (const key in mobile1) {
+    console.log(mobile1[key]);
+    for(const key1 in mobile2){
+        console.log(mobile2[key1]);
+    }
+}
 // let obj = {
 //     firstName: 'Ammar',
 //     lastName: 'Ahmed',
@@ -43,7 +55,7 @@
 //     bookDetails: {
 //         Aurthor: 'Alice Heprew',
 //         bookName: 'Smater Way To Learn JS'
-//     },
+//     },   
 //     getBookName: function () {
 //         return (`We are using the book to study is ${obj.bookDetails.bookName} and the Aurthor of this book is ${obj.bookDetails.Aurthor}`);
 //     },
@@ -66,9 +78,16 @@
 //  }
 // obj[keys] it returs us the value stored in the object
 // key  it gives us the variable in the object;
+let btn = document.getElementById('bt');
+let hell = "";
+hell = () => {
+    document.getElementById("demo").innerHTML += this;
+}
+//The window object calls the function:
+btn.addEventListener('click', hell);
+
 //Arrow function
 let multiply = (a, b) => a * b;
-console.log(`Arrow func ${multiply(2, 4)} *`);
 
 let add = (a, b) => a + b;
 console.log(`Arrow function ${add(2, 4)} +`);
@@ -76,7 +95,7 @@ console.log(`Arrow function ${add(2, 4)} +`);
 
 // This is the long way to do 
 let hello = "";
-hello = () =>{
+hello = () => {
     return "How are you ?"
 }
 console.log(hello());
